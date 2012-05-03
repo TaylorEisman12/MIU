7bc832a9aa44bfbb0522a0fc897f630b8fbfb471
Taylor Eisman <TaylorEisman@fullsail.edu>
2012-05-03 14:50:34 -0500

    added bills 4-8.

--- javascript/json.js
+++ javascript/json.js
@@ -36,4 +36,59 @@ var json = {
 		"date": ["Date Added:", "2011-12-21"],
 		"notes": ["Notes:", "Call office."]
 	}
+		"bill4": {
+		"category": ["Category:", "Cable"],
+		"compName": ["Comapany Name:", "Time Warner"],
+		"compEmail": ["Company Email:", "timewarner@time.com"],
+		"compWeb": ["Company Website:", "www.timewarner.com"],
+		"payBy": ["Pay By:", "Internet"],
+		"pastDue": ["Past Due:", "Yes"],
+		"budgetPercent": ["Budget Percent:", "25"],
+		"date": ["Date Added:", "2011-02-11"],
+		"notes": ["Notes:", "Check for faster internet."]
+	}
+		"bill5": {
+		"category": ["Category:", "Cell"],
+		"compName": ["Comapany Name:", "Tmobile"],
+		"compEmail": ["Company Email:", "Tmobile@mobile.com"],
+		"compWeb": ["Company Website:", "www.Tmobile.com"],
+		"payBy": ["Pay By:", "Mail"],
+		"pastDue": ["Past Due:", "No"],
+		"budgetPercent": ["Budget Percent:", "15"],
+		"date": ["Date Added:", "2012-11-11"],
+		"notes": ["Notes:", "Daughterss cell phone."]
+	}
+		"bill6": {
+		"category": ["Category:", "Cell"],
+		"compName": ["Comapany Name:", "Sprint"],
+		"compEmail": ["Company Email:", "sprint@mobile.com"],
+		"compWeb": ["Company Website:", "www.Sprint.com"],
+		"payBy": ["Pay By:", "Internet"],
+		"pastDue": ["Past Due:", "No"],
+		"budgetPercent": ["Budget Percent:", "15"],
+		"date": ["Date Added:", "2012-09-11"],
+		"notes": ["Notes:", "Son's cell phone."]
+	}
+		"bill7": {
+		"category": ["Category:", "Rent"],
+		"compName": ["Comapany Name:", "Park Place"],
+		"compEmail": ["Company Email:", "parkplace@healthcare.com"],
+		"compWeb": ["Company Website:", "www.parkplace.com"],
+		"payBy": ["Pay By:", "Mail"],
+		"pastDue": ["Past Due:", "Yes"],
+		"budgetPercent": ["Budget Percent:", "75"],
+		"date": ["Date Added:", "2011-04-01"],
+		"notes": ["Notes:", "Mom's rest home."]
+	}
+		"bill8": {
+		"category": ["Category:", "Cable"],
+		"compName": ["Comapany Name:", "Direct Tv"],
+		"compEmail": ["Company Email:", "direct@tv.com"],
+		"compWeb": ["Company Website:", "www.directtv.com"],
+		"payBy": ["Pay By:", "Internet"],
+		"pastDue": ["Past Due:", "No"],
+		"budgetPercent": ["Budget Percent:", "25"],
+		"date": ["Date Added:", "2012-07-25"],
+		"notes": ["Notes:", "Son's dorm room."]
+	}
 }
